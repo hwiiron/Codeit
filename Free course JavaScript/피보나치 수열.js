f035f984e399cfd60a4cc8042bb5a186f13c685a
shinhwichul <shinhwiiron@gmail.com>
2024-08-30 22:41:56 +0900

    Free course JavaScript 기초 실습 소스 코드 추가

---
+++ Free course JavaScript/피보나치 수열.js
@@ -0,0 +1,11 @@
+// 피보나치 수열
+
+let previous = 0;
+let current = 1;
+
+for (let i = 1; i < 50; i++) {
+  console.log(current);
+  future = previous + current;
+  previous = current;
+  current = future;
+}
