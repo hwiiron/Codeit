3d47bc4f0c59246ba0f2c69879c64af26ca414be
Hwichul Shin <shinhwiiron@gmail.com>
2024-09-01 19:35:43 +0900

    피보나치 수열 반복문 수정

--- Free course JavaScript/피보나치 수열.js
+++ Free course JavaScript/피보나치 수열.js
@@ -3,7 +3,7 @@
 let previous = 0;
 let current = 1;
 
-for (let i = 1; i < 50; i++) {
+for (let i = 1; i <= 50; i++) {
   console.log(current);
   future = previous + current;
   previous = current;
