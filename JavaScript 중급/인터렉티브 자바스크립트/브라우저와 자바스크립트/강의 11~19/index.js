ddf2ed9b8b82bf658d57c537cedecd8e37a44849
Hwichul Shin <shinhwiiron@gmail.com>
2024-09-08 11:42:27 +0900

    브라우저 자바스크립트 실습 소스 코드 추가

--- JavaScript 중급/인터렉티브 자바스크립트/브라우저와 자바스크립트/강의 11~19/index.js
+++ JavaScript 중급/인터렉티브 자바스크립트/브라우저와 자바스크립트/강의 11~19/index.js
@@ -4,7 +4,7 @@ const tomorrow = document.querySelector('#tomorrow');
 // 1. 요소 노드 만들기: document.createElement('태그이름');
 const first = document.createElement('li');
 
-// 2. 요소노드 꾸미기 : textContent, innerHTML, ...
+// 2. 요소 노드 꾸미기 : textContent, innerHTML, ...
 first.textContent = '처음';
 
 // 3. 요소 노드 추가하기: NODE.prepend, append, after, before
@@ -21,3 +21,18 @@ tomorrow.before(prev); // tomorrow element 이전 추가
 const next = document.createElement('p');
 next.textContent = '다음';
 tomorrow.after(next); // tomorrow element 다음 추가
+
+
+// 노드 삭제와 이동
+const today = document.querySelector('#today');
+const tomorrow2 = document.querySelector('#tomorrow');
+
+// 노드 삭제하기 : Node.remove()
+// tomorrow.remove();
+// today.children[2].remove();
+
+
+// 노드 이동하기: prepend, append, before, after
+// today.append(tomorrow.children[1]);
+
+tomorrow.children[1].after(today.children[1]);
