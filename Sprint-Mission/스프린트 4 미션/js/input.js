e45a1602ece8b1e7bce47497cb9dae42190b4456
Hwichul Shin <shinhwiiron@gmail.com>
2024-09-24 08:37:20 +0900

    변수명, class명 전체적으로 수정, 로직 최소화

--- Sprint-Mission/스프린트 4 미션/js/input.js
+++ Sprint-Mission/스프린트 4 미션/js/input.js
@@ -1,35 +1,34 @@
 // 아이콘(눈) 버튼 클릭 시 비밀번호 보이기 / 숨기기
-const eye = document.querySelector('.eye');
-const password = document.querySelector('#password-input');
+const visibilityToggleButton = document.querySelector('.eye');
+const passwordInput = document.querySelector('#password-input');
 
 function passwordShow() {
-  if (password.getAttribute('type') == 'password') {
-    eye.classList.add('show');
-    password.setAttribute('type', 'text');
-  } else {
-    eye.classList.remove('show');
-    password.setAttribute('type', 'password');
-  }
+  const isPasswordHidden = passwordInput.getAttribute('type') === 'password';
+  visibilityToggleButton.classList.toggle('show', isPasswordHidden);  // 조건에 따라 class 토글
+  passwordInput.setAttribute('type', isPasswordHidden ? 'text' : 'password'); 
 }
 
 
 
 // 이메일 형식 체크
 const pattern = /^[A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Za-z0-9]+/;
-const email = document.querySelector('#email-input');
+// @을 기준으로 앞 구간이 대, 소문자 or 숫자 조합으로 이루어져 있는지 체크
+// @을 기준으로 뒷 구간이 대, 소문자 or 숫자 조합으로 이루어져 있는지 체크
+// @을 기준으로 뒷 구간에서 .뒷 구간이 대, 소문자 or 숫자 조합으로 이루어져 있는지
+
+const emailInput = document.querySelector('#email-input');
 const msg = document.querySelector('.emailMsg');
 const errMsg = document.querySelector('.emailErrMsg');
 
 function emailCheck() {
-  if (email.value == '') { // 입력 값이 없을 때
+  // 모든 메시지 숨김
+  msg.classList.remove('show');
+  errMsg.classList.remove('show');
+
+  if (emailInput.value == '') { // 입력 값이 없을 때
     msg.classList.add('show');
-    errMsg.classList.remove('show');
-  } else if (!pattern.test(email.value)) { // 입력 값은 있으나 이메일 형식에 맞지 않을 때
+  } else if (!pattern.test(emailInput.value)) { // 입력 값은 있으나 이메일 형식에 맞지 않을 때
     errMsg.classList.add('show');
-    msg.classList.remove('show');
-  } else {
-    msg.classList.remove('show');
-    errMsg.classList.remove('show');
   }
 }
 
@@ -40,15 +39,13 @@ const passwordMsg = document.querySelector('.passwordMsg');
 const passwordLength = document.querySelector('.passwordLength');
 
 function passwordCheck() {
-  if (password.value == '') {
+  passwordLength.classList.remove('show');
+  passwordMsg.classList.remove('show');
+
+  if (passwordInput.value == '') {
     passwordMsg.classList.add('show');
-    passwordLength.classList.remove('show');
-  } else if (password.value.length < 8) {
+  } else if (passwordInput.value.length < 8) {
     passwordLength.classList.add('show');
-    passwordMsg.classList.remove('show');
-  } else {
-    passwordLength.classList.remove('show');
-    passwordMsg.classList.remove('show');
   }
 }
 
@@ -57,21 +54,17 @@ function passwordCheck() {
 // 이메일, 비밀번호 입력 시 로그인 버튼 활성화 / 비활성화
 const loginBtn = document.querySelector('.loginBtn');
 
-function loginBtnActive () {
-  if (pattern.test(email.value) && password.value.length >= 8) {
-    loginBtn.classList.add('active');
-    loginBtn.disabled = false;
-  } else {
-    loginBtn.classList.remove('active');
-    loginBtn.disabled = true;
-  }
+function activateLoginBtnState () {
+  const LoginFormValid = pattern.test(emailInput.value) && passwordInput.value.length >= 8;
+  loginBtn.classList.toggle('activate', LoginFormValid);
+  loginBtn.disabled = !LoginFormValid;
 }
 
 
 
 // 이메일, 비밀번호 입력 후 엔터키 누르면 로그인 버튼 클릭
-function loginEnter(e) {
-  if (e.keyCode == 13 && pattern.test(email.value) && password.value.length >= 8) {
+function loginOnEnter(e) {
+  if (e.keyCode == 13 && pattern.test(emailInput.value) && passwordInput.value.length >= 8) {
     loginBtn.click();
   }
 }
@@ -79,15 +72,12 @@ function loginEnter(e) {
 
 
 // 닉네임 체크
-const nickName = document.querySelector('#nickname-input');
+const nickNameInput = document.querySelector('#nickname-input');
 const nicknameMsg = document.querySelector('.nicknameMsg');
 
 function nickNameCheck() {
-  if (nickName.value == '') {
-    nicknameMsg.classList.add('show');
-  } else {
-    nicknameMsg.classList.remove('show');
-  }
+  const isNickNameEmpty = nickNameInput.value == '';
+  nicknameMsg.classList.toggle('show', isNickNameEmpty);
 }
 
 
@@ -97,11 +87,8 @@ const passwordRepeat = document.querySelector('#passwordCheck-input');
 const passwordCheckMsg = document.querySelector('.passwordCheckMsg');
 
 function passwordConfirm() {
-  if (password.value !== passwordRepeat.value) {
-    passwordCheckMsg.classList.add('show');
-  } else {
-    passwordCheckMsg.classList.remove('show');
-  }
+  const passwordMatching = passwordInput.value !== passwordRepeat.value;
+  passwordCheckMsg.classList.toggle('show', passwordMatching);
 }
 
 
@@ -109,25 +96,25 @@ function passwordConfirm() {
 // 이메일, 닉네임, 비밀번호, 비밀번호 확인 입력 시 로그인 버튼 활성화 / 비활성화
 const signupBtn = document.querySelector('.signupBtn');
 
-function signupBtnActive () {
-  if (pattern.test(email.value) && nickName.value !== '' && password.value.length >= 8 && password.value == passwordRepeat.value) {
-    signupBtn.classList.add('active');
-    signupBtn.disabled = false;
-  } else {
-    signupBtn.classList.remove('active');
-    signupBtn.disabled = true;
-  }
+function activateSignupBtnState () {
+  const signupFormValid = pattern.test(emailInput.value) &&
+                          nickNameInput.value !== '' &&
+                          passwordInput.value.length >= 8 &&
+                          passwordInput.value == passwordRepeat.value;
+
+  signupBtn.classList.toggle('activate', signupFormValid);
+  signupBtn.disabled = !signupFormValid;
 }
 
 
 
 // 이메일, 닉네임, 비밀번호, 비밀번호 확인 입력 후 엔터키 누르면 로그인 버튼 클릭
-function signupEnter(e) {
-  if (e.keyCode == 13 && pattern.test(email.value) && nickName.value !== '' && password.value.length >= 8 && password.value == passwordRepeat.value) {
+function signupOnEnter(e) {
+  if (e.keyCode == 13 && pattern.test(emailInput.value) && nickNameInput.value !== '' && passwordInput.value.length >= 8 && passwordInput.value == passwordRepeat.value) {
     signupBtn.click();
   }
 }
 
 
 
-export { eye, email, password, nickName, passwordRepeat, signupBtn, passwordShow, emailCheck, passwordCheck, loginBtnActive, loginEnter, nickNameCheck, passwordConfirm, signupBtnActive, signupEnter }
+export { visibilityToggleButton, emailInput, passwordInput, nickNameInput, passwordRepeat, signupBtn, passwordShow, emailCheck, passwordCheck, activateLoginBtnState, loginOnEnter, nickNameCheck, passwordConfirm, activateSignupBtnState, signupOnEnter }
