8ce79f08f99556c8ca52fea816b8d0ea44635d63
Hwichul Shin <shinhwiiron@gmail.com>
2024-09-24 09:38:21 +0900

    input 이벤트 제거

--- Sprint-Mission/스프린트 4 미션/js/login.js
+++ Sprint-Mission/스프린트 4 미션/js/login.js
@@ -1,17 +1,15 @@
-import { visibilityToggleButton, emailInput, passwordInput, passwordShow, emailCheck, passwordCheck, activateLoginBtnState, loginOnEnter } from './input.js';
+import { passwordToggleBtn, emailInput, passwordInput, passwordShow, emailCheck, passwordCheck, activateLoginBtnState, loginOnEnter } from './input.js';
 
 // 아이콘(눈) 버튼 클릭 시 비밀번호 보이기 / 숨기기
-visibilityToggleButton.addEventListener('click', passwordShow);
+passwordToggleBtn.addEventListener('click', passwordShow);
 
 
 // 이메일 형식 체크
 emailInput.addEventListener('focusout', emailCheck);
-emailInput.addEventListener('input', emailCheck);
 
 
 // 비밀번호 체크
 passwordInput.addEventListener('focusout', passwordCheck);
-passwordInput.addEventListener('input', passwordCheck);
 
 
 // 이메일, 비밀번호 입력 시 로그인 버튼 활성화 / 비활성화
