3fa7d584dfda73e67e0abc25ccb722320ae55f17
Hwichul Shin <shinhwiiron@gmail.com>
2024-10-02 16:56:29 +0900

    Count app 추가

--- onebite-react/counter-app/src/App.jsx
+++ onebite-react/counter-app/src/App.jsx
@@ -1,8 +1,26 @@
 import { useState } from "react";
 import "./App.css";
+import Viewer from "./assets/Viewer";
+import Controller from "./assets/Controller";
 
 function App() {
-  return <>카운터 앱</>;
+  const [count, setCount] = useState(0);
+
+  const onClickButton = (value) => {
+    setCount(count + value);
+  };
+
+  return (
+    <div>
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer count={count} />
+      </section>
+      <section>
+        <Controller onClickButton={onClickButton} />
+      </section>
+    </div>
+  );
 }
 
 export default App;
---
+++ onebite-react/counter-app/src/assets/Viewer.jsx
@@ -0,0 +1,10 @@
+const Viewer = ({ count }) => {
+  return (
+    <div>
+      <div>햔제 키은트 :</div>
+      <h1>{count}</h1>
+    </div>
+  );
+};
+
+export default Viewer;
