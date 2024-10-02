6500def15ad0fcec2b13219ea4e25486f4494996
Hwichul Shin <shinhwiiron@gmail.com>
2024-10-02 09:31:50 +0900

    sort() 메소드로 최신순, 칼로리순 정렬 버튼 추가

--- React/foodit/src/components/FoodList.js
+++ React/foodit/src/components/FoodList.js
@@ -1,27 +1,32 @@
+import "./FoodList.css";
+
+function formatDate(value) {
+  const date = new Date(value);
+  return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
+}
+
 function FoodListItem({ item }) {
-  const { imgUrl, title, calorie, content } = item;
+  const { imgUrl, title, calorie, content, createdAt } = item;
 
   return (
-    <div>
+    <div className="FoodListItem">
       <img src={imgUrl} alt={title} />
       <div>{title}</div>
       <div>{calorie}</div>
       <div>{content}</div>
+      <div>{formatDate(createdAt)}</div>
     </div>
   );
 }
 
 function FoodList({ items }) {
-  console.log(items);
   return (
-    <ul>
-      {items.map((item) => {
-        return (
-          <li>
-            <FoodListItem item={item} />
-          </li>
-        );
-      })}
+    <ul className="FoodList">
+      {items.map((item) => (
+        <li>
+          <FoodListItem item={item} />
+        </li>
+      ))}
     </ul>
   );
 }
