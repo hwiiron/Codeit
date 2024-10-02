c09011c6ea80e46b80dd605f971fb7b992dac9b3
Hwichul Shin <shinhwiiron@gmail.com>
2024-10-02 11:10:00 +0900

    filter()메소드로 삭제 버튼 구현

--- React/foodit/src/components/App.js
+++ React/foodit/src/components/App.js
@@ -1,19 +1,26 @@
-import FoodList from "./FoodList";
-import items from "../mock.json";
 import { useState } from "react";
+import FoodList from "./FoodList";
+import mockList from "../mock.json";
 
 function App() {
+  const [items, setItems] = useState(mockList);
   const [order, setOrder] = useState("createdAt");
-  const sortedList = items.sort((a, b) => b[order] - a[order]);
 
   const handleNewestClick = () => setOrder("createdAt");
   const handleCalorieClick = () => setOrder("calorie");
+
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleDelete = (id) => {
+    const newitems = items.filter((item) => item.id !== id);
+    setItems(newitems);
+  };
 
   return (
     <div>
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleCalorieClick}>칼로리순</button>
-      <FoodList items={sortedList} />
+      <FoodList items={sortedItems} onDelete={handleDelete} />
     </div>
   );
 }
