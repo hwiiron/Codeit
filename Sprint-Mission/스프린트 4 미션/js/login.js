0e7f88cd2016b32b98310cff342ca3616d1c2268
Hwichul Shin <shinhwiiron@gmail.com>
2024-10-03 19:18:09 +0900

    변수, 함수명 수정

--- Sprint-Mission/스프린트 4 미션/js/login.js
+++ Sprint-Mission/스프린트 4 미션/js/login.js
@@ -3,30 +3,25 @@ import {
   emailInput,
   passwordInput,
   passwordToggleVisibility,
-  emailCheck,
-  passwordCheck,
+  checkEmail,
+  checkPassword,
   activateLoginBtnState,
-  loginOnEnter
-} from './validation.js';
-
+  loginOnEnter,
+} from "./validation.js";
 
 // 이메일 형식 체크
-emailInput.addEventListener('focusout', emailCheck);
-emailInput.addEventListener('input', emailCheck);
-
+emailInput.addEventListener("focusout", checkEmail);
+emailInput.addEventListener("input", checkEmail);
 
 // 비밀번호 체크
-passwordInput.addEventListener('focusout', passwordCheck);
-passwordInput.addEventListener('input', passwordCheck);
-
+passwordInput.addEventListener("focusout", checkPassword);
+passwordInput.addEventListener("input", checkPassword);
 
 // 아이콘(눈) 버튼 클릭 시 비밀번호 보이기 / 숨기기
-passwordToggleBtn.addEventListener('click', passwordToggleVisibility);
-
+passwordToggleBtn.addEventListener("click", passwordToggleVisibility);
 
 // 이메일, 비밀번호 입력 시 로그인 버튼 활성화 / 비활성화
-document.addEventListener('input', activateLoginBtnState);
-
+document.addEventListener("input", activateLoginBtnState);
 
 // 이메일, 비밀번호 입력 후 엔터키 누르면 로그인 버튼 클릭
-document.addEventListener('keydown', loginOnEnter);
+document.addEventListener("keydown", loginOnEnter);
--- Sprint-Mission/스프린트 4 미션/js/signup.js
+++ Sprint-Mission/스프린트 4 미션/js/signup.js
@@ -7,46 +7,38 @@ import {
   passwordCheckInput,
   passwordToggleVisibility,
   passwordCheckToggleVisibility,
-  emailCheck,
-  passwordCheck,
+  checkEmail,
+  checkPassword,
   nickNameCheck,
-  passwordConfirm,
+  confirmPassword,
   activateSignupBtnState,
-  signupOnEnter
-} from './validation.js';
-
+  signupOnEnter,
+} from "./validation.js";
 
 // 이메일 형식 체크
-emailInput.addEventListener('focusout', emailCheck);
-emailInput.addEventListener('input', emailCheck);
-
+emailInput.addEventListener("focusout", checkEmail);
+emailInput.addEventListener("input", checkEmail);
 
 // 비밀번호 체크
-passwordInput.addEventListener('focusout', passwordCheck);
-passwordInput.addEventListener('input', passwordCheck);
-
+passwordInput.addEventListener("focusout", checkPassword);
+passwordInput.addEventListener("input", checkPassword);
 
 // 닉네임 체크
-nickNameInput.addEventListener('focusout', nickNameCheck);
-nickNameInput.addEventListener('input', nickNameCheck);
-
+nickNameInput.addEventListener("focusout", nickNameCheck);
+nickNameInput.addEventListener("input", nickNameCheck);
 
 // 비밀번호 확인 체크
-passwordCheckInput.addEventListener('focusout', passwordConfirm);
-passwordCheckInput.addEventListener('input', passwordConfirm);
-
+passwordCheckInput.addEventListener("focusout", confirmPassword);
+passwordCheckInput.addEventListener("input", confirmPassword);
 
 // 비밀번호 - 아이콘(눈) 버튼 클릭 시 비밀번호 보이기 / 숨기기
-passwordToggleBtn.addEventListener('click', passwordToggleVisibility);
-
+passwordToggleBtn.addEventListener("click", passwordToggleVisibility);
 
 // 비밀번호 확인 - 아이콘(눈) 버튼 클릭 시 비밀번호 보이기 / 숨기기
-passwordCheckToggleBtn.addEventListener('click', passwordCheckToggleVisibility);
-
+passwordCheckToggleBtn.addEventListener("click", passwordCheckToggleVisibility);
 
 // 이메일, 비밀번호 입력 시 로그인 버튼 활성화 / 비활성화
-document.addEventListener('input', activateSignupBtnState);
-
+document.addEventListener("input", activateSignupBtnState);
 
 // 이메일, 닉네임, 비밀번호, 비밀번호 확인 입력 후 엔터키 누르면 로그인 버튼 클릭
-document.addEventListener('keydown', signupOnEnter);
+document.addEventListener("keydown", signupOnEnter);
