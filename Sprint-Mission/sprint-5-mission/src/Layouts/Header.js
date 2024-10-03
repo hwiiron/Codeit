7b1390a926e7fbf18530ca4eac776de5b1d88488
Hwichul Shin <shinhwiiron@gmail.com>
2024-10-03 17:16:39 +0900

    메인, 서브 header 분리

--- Sprint-Mission/sprint-5-mission/src/Layouts/Header.js
+++ Sprint-Mission/sprint-5-mission/src/Layouts/Header.js
@@ -1,38 +1,17 @@
 import "./Header.css";
-// import profileImg from "../assets/profileImg.svg";
+import profileImg from "../assets/profileImg.svg";
 
-function Header() {
+export function MainHeader() {
   return (
-    // <header>
-    //   <div className="header__inner">
-    //     <h1>
-    //       <a className="header__logo" href="/"></a>
-    //     </h1>
-
-    //     <ul className="header__nav">
-    //       <li>
-    //         <a href="">자유게시판</a>
-    //       </li>
-
-    //       <li>
-    //         <a href="">중고마켓</a>
-    //       </li>
-    //     </ul>
-
-    //     <a href="" className="header__profile">
-    //       <img src={profileImg} alt="프로필 이미지" />
-    //     </a>
-    //   </div>
-    // </header>
-    <header>
-      <div class="inner">
+    <header className="MainHeader">
+      <div className="inner">
         <h1>
-          <a class="logo" href="/">
+          <a className="logo" href="/">
             로고
           </a>
         </h1>
 
-        <a class="login" href="login.html">
+        <a className="login" href="login.html">
           로그인
         </a>
       </div>
@@ -40,4 +19,28 @@ function Header() {
   );
 }
 
-export default Header;
+export function SubHeader() {
+  return (
+    <header className="SubHeader">
+      <div className="SubHeader__inner">
+        <h1>
+          <a className="SubHeader__logo" href="/"></a>
+        </h1>
+
+        <ul className="SubHeader__nav">
+          <li>
+            <a href="">자유게시판</a>
+          </li>
+
+          <li>
+            <a href="">중고마켓</a>
+          </li>
+        </ul>
+
+        <a href="#" className="SubHeader__profile">
+          <img src={profileImg} alt="프로필 이미지" />
+        </a>
+      </div>
+    </header>
+  );
+}
