878fd0e0e7d146b0b935f8fe6a666d4613cf898a
Hwichul Shin <shinhwiiron@gmail.com>
2024-10-05 13:57:36 +0900

    useState, handleChange 공통

--- React/moviepedia/src/components/ReviewForm.js
+++ React/moviepedia/src/components/ReviewForm.js
@@ -2,28 +2,53 @@ import { useState } from "react";
 import "./ReviewForm.css";
 
 const ReviewForm = () => {
-  const [title, setTitle] = useState("");
-  const [rating, setRating] = useState(0);
-  const [content, setContent] = useState("");
+  // const [title, setTitle] = useState("");
+  // const [rating, setRating] = useState(0);
+  // const [content, setContent] = useState("");
 
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
+  // const handleTitleChange = (e) => {
+  //   setTitle(e.target.value);
+  // };
+
+  // const handleRatingChange = (e) => {
+  //   const nextRating = Number(e.target.value) || 0;
+  //   setRating(nextRating);
+  // };
+
+  // const handleContentChange = (e) => {
+  //   setContent(e.target.value);
+  // };
+
+  const [values, setValues] = useState({
+    title: "",
+    rating: 0,
+    content: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
   };
 
-  const handleRatingChange = (e) => {
-    const nextRating = Number(e.target.value) || 0;
-    setRating(nextRating);
-  };
-
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
   };
 
   return (
-    <form className="ReviewForm">
-      <input value={title} onChange={handleTitleChange} />
-      <input type="number" value={rating} onChange={handleRatingChange} />
-      <textarea value={content} onChange={handleContentChange} />
+    <form className="ReviewForm" onSubmit={handleSubmit}>
+      <input name="title" value={values.title} onChange={handleChange} />
+      <input
+        name="rating"
+        type="number"
+        value={values.rating}
+        onChange={handleChange}
+      />
+      <textarea name="content" value={values.content} onChange={handleChange} />
+      <button type="submit">확인</button>
     </form>
   );
 };
