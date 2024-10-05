a00feda012387846825c4206b39749a19246c27f
Hwichul Shin <shinhwiiron@gmail.com>
2024-10-05 14:09:27 +0900

    useState, handleChange 공통

--- React/foodit/src/components/FoodForm.js
+++ React/foodit/src/components/FoodForm.js
@@ -1,37 +1,57 @@
 import { useState } from "react";
 
 function FoodForm() {
-  const [title, setTitle] = useState("");
-  const [calorie, setCalorie] = useState(0);
-  const [content, setContent] = useState("");
+  // const [title, setTitle] = useState("");
+  // const [calorie, setCalorie] = useState(0);
+  // const [content, setContent] = useState("");
 
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
+  // const handleTitleChange = (e) => {
+  //   setTitle(e.target.value);
+  // };
+
+  // const handleCalorieChange = (e) => {
+  //   const nextRating = Number(e.target.value) || 0;
+  //   setCalorie(nextRating);
+  // };
+
+  // const handleContentChange = (e) => {
+  //   setContent(e.target.value);
+  // };
+
+  const [values, setValues] = useState({
+    title: "",
+    calorie: 0,
+    content: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
   };
 
-  const handleCalorieChange = (e) => {
-    const nextRating = Number(e.target.value) || 0;
-    setCalorie(nextRating);
-  };
-
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
   };
 
   return (
-    <form>
-      <input name="title" value={title} onChange={handleTitleChange}></input>
+    <form onSubmit={handleSubmit}>
+      <input name="title" value={values.title} onChange={handleChange}></input>
       <input
         type="number"
         name="calorie"
-        value={calorie}
-        onChange={handleCalorieChange}
+        value={values.calorie}
+        onChange={handleChange}
       ></input>
       <input
         name="content"
-        value={content}
-        onChange={handleContentChange}
+        value={values.content}
+        onChange={handleChange}
       ></input>
+      <button type="submit">확인</button>
     </form>
   );
 }
