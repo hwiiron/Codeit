b6c1f702f56883405ed972e2ceda1cbce5da8f52
Hwichul Shin <shinhwiiron@gmail.com>
2024-10-14 12:02:44 +0900

    useCallback 적용

--- onebite-react/todo-list/src/App.jsx
+++ onebite-react/todo-list/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState, useRef, useReducer } from "react";
+import { useState, useRef, useReducer, useCallback } from "react";
 import Editor from "./components/Editor";
 import Header from "./components/Header";
 import List from "./components/List";
@@ -44,7 +44,7 @@ function App() {
   const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3);
 
-  const onCreate = (content) => {
+  const onCreate = useCallback((content) => {
     // const newTodo = {
     //   id: idRef.current++,
     //   isDone: false,
@@ -61,9 +61,9 @@ function App() {
         date: new Date().getTime(),
       },
     });
-  };
+  }, []);
 
-  const onUpdate = (targetId) => {
+  const onUpdate = useCallback((targetId) => {
     // todos State의 값들 중에
     // targetId와 일치하는 id를 갖는 아이템의 isDone 변경
 
@@ -84,17 +84,24 @@ function App() {
       type: "UPDATE",
       targetId: targetId,
     });
-  };
+  }, []);
 
-  const onDelete = (targetId) => {
-    // 인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소만 삭제한 새로운 배열
-    // setTodos(todos.filter((todo) => todo.id !== targetId));
+  // const onDelete = (targetId) => {
+  //   인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소만 삭제한 새로운 배열
+  //   setTodos(todos.filter((todo) => todo.id !== targetId));
 
+  //   dispatch({
+  //     type: "DELETE",
+  //     targetId: targetId,
+  //   });
+  // };
+
+  const onDelete = useCallback((targetId) => {
     dispatch({
       type: "DELETE",
       targetId: targetId,
     });
-  };
+  }, []);
 
   return (
     <div className="App">
