fa03e84ff38cc7ca949bd54c6dab59e461e4470a
Hwichul Shin <shinhwiiron@gmail.com>
2024-10-16 20:26:55 +0900

    Home 페이지 구현

--- onebite-react/emotion-diary/src/App.jsx
+++ onebite-react/emotion-diary/src/App.jsx
@@ -11,15 +11,21 @@ import NotFound from "./pages/NotFound";
 const mockData = [
   {
     id: 1,
-    createDate: new Date().getTime(),
+    createdDate: new Date("2024-10-16").getTime(),
     emotionId: 1,
     content: "1번 일기 내용",
   },
   {
     id: 2,
-    createDate: new Date().getTime(),
+    createdDate: new Date("2024-10-15").getTime(),
     emotionId: 2,
     content: "2번 일기 내용",
+  },
+  {
+    id: 3,
+    createdDate: new Date("2024-09-15").getTime(),
+    emotionId: 3,
+    content: "3번 일기 내용",
   },
 ];
 
@@ -41,8 +47,8 @@ function reducer(state, action) {
   }
 }
 
-const DiaryStateContext = createContext();
-const DiaryDispatchContext = createContext();
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
