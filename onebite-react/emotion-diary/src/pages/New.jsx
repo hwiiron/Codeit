6210a3d5235bd0c7a4ba6b1e73bbd2bf6bd84933
Hwichul Shin <shinhwiiron@gmail.com>
2024-10-17 14:39:03 +0900

    일기 생성 페이지

--- onebite-react/emotion-diary/src/pages/New.jsx
+++ onebite-react/emotion-diary/src/pages/New.jsx
@@ -1,5 +1,29 @@
+import Button from "../components/Button";
+import Header from "../components/Header";
+import Editor from "../components/Editor";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
+
 const New = () => {
-  return <div>New</div>;
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const nav = useNavigate();
+
+  const onsubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    nav("/", { replace: true });
+  };
+
+  return (
+    <>
+      <Header
+        title={"새 일기 쓰기"}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
+      />
+
+      <Editor onSubmit={onsubmit} />
+    </>
+  );
 };
 
 export default New;
