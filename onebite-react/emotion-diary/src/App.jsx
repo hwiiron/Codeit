009b1ee4f085d3cef770852b41d7492daf3da12f
Hwichul Shin <shinhwiiron@gmail.com>
2024-10-21 11:21:22 +0900

    로컬 스토리지

--- onebite-react/emotion-diary/src/App.jsx
+++ onebite-react/emotion-diary/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
-import { useReducer, useRef, createContext } from "react";
+import { useReducer, useRef, createContext, useEffect, useState } from "react";
 
 import Home from "./pages/Home";
 import New from "./pages/New";
@@ -8,52 +8,98 @@ import Diary from "./pages/Diary";
 import Edit from "./pages/Edit";
 import NotFound from "./pages/NotFound";
 
-const mockData = [
-  {
-    id: 1,
-    createdDate: new Date("2024-10-16").getTime(),
-    emotionId: 1,
-    content: "1번 일기 내용",
-  },
-  {
-    id: 2,
-    createdDate: new Date("2024-10-15").getTime(),
-    emotionId: 2,
-    content: "2번 일기 내용",
-  },
-  {
-    id: 3,
-    createdDate: new Date("2024-09-15").getTime(),
-    emotionId: 3,
-    content: "3번 일기 내용",
-  },
-];
+// const mockData = [
+//   {
+//     id: 1,
+//     createdDate: new Date("2024-10-16").getTime(),
+//     emotionId: 1,
+//     content: "1번 일기 내용",
+//   },
+//   {
+//     id: 2,
+//     createdDate: new Date("2024-10-15").getTime(),
+//     emotionId: 2,
+//     content: "2번 일기 내용",
+//   },
+//   {
+//     id: 3,
+//     createdDate: new Date("2024-09-15").getTime(),
+//     emotionId: 3,
+//     content: "3번 일기 내용",
+//   },
+// ];
 
 function reducer(state, action) {
-  switch (action.type) {
-    case "CREATE":
-      return [action.data, ...state];
+  let nextState;
 
-    case "UPDATE":
-      return state.map((item) =>
+  switch (action.type) {
+    case "INIT":
+      return action.data;
+
+    case "CREATE": {
+      nextState = [action.data, ...state];
+      break;
+    }
+    case "UPDATE": {
+      nextState = state.map((item) =>
         String(item.id) === String(action.data.id) ? action.data : item
       );
-
-    case "DELETE":
-      return state.filter((item) => String(item.id) !== String(action.id));
-
+      break;
+    }
+    case "DELETE": {
+      {
+        nextState = state.filter(
+          (item) => String(item.id) !== String(action.id)
+        );
+        break;
+      }
+    }
     default:
       return state;
   }
+
+  localStorage.setItem("diary", JSON.stringify(nextState));
+  return nextState;
 }
 
 export const DiaryStateContext = createContext();
 export const DiaryDispatchContext = createContext();
 
 function App() {
-  const [data, dispatch] = useReducer(reducer, mockData);
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, dispatch] = useReducer(reducer, []);
 
-  const idRef = useRef(3);
+  const idRef = useRef(0);
+
+  useEffect(() => {
+    const storedData = localStorage.getItem("diary");
+    if (!storedData) {
+      setIsLoading(false);
+      return;
+    }
+
+    const parsedData = JSON.parse(storedData);
+    if (!Array.isArray(parsedData)) {
+      setIsLoading(false);
+      return;
+    }
+
+    let maxId = 0;
+    parsedData.forEach((item) => {
+      if (Number(item.id) > maxId) {
+        maxId = Number(item.id);
+      }
+    });
+
+    idRef.current = maxId + 1;
+
+    dispatch({
+      type: "INIT",
+      data: parsedData,
+    });
+
+    setIsLoading(false);
+  }, []);
 
   // 새로운 일기 추가
   const onCreate = (createdDate, emotionId, content) => {
@@ -89,6 +135,10 @@ function App() {
       id,
     });
   };
+
+  if (isLoading) {
+    return <div>데이터 로딩중입니다...</div>;
+  }
 
   return (
     <>
