9d7a51000426cad7d357e2235aeaf336be709c54
Hwichul Shin <shinhwiiron@gmail.com>
2024-11-12 13:42:58 +0900

    객체

---
+++ onebite-typescript/section2/src/chapter3.ts
@@ -0,0 +1,16 @@
+// object
+let user: {
+  id?: number;
+  name: string;
+} = {
+  id: 1,
+  name: "신휘철",
+};
+
+let config: {
+  readonly apiKey: string;
+} = {
+  apiKey: "MY API KEY",
+};
+
+// config.apiKey = "hacked";
