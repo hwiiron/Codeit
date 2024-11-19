fced106ec2193c7e3d51843506cfc6a5e438b376
Hwichul Shin <shinhwiiron@gmail.com>
2024-11-19 13:54:36 +0900

    Next.js 수업 코드 추가

--- next/pages/index.js
+++ next/pages/index.js
@@ -1,25 +1,26 @@
-import Link from "next/link";
-
-// import styles from "@/styles/Home.module.css";
+import { useEffect, useState } from "react";
+import axios from "../lib/axios";
+import ProductList from "@/components/ProductList";
+import SearchForm from "@/components/SearchForm";
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  async function getProducts() {
+    const res = await axios.get("/products");
+    const nextProducts = res.data.results;
+    setProducts(nextProducts);
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
-    <div>
+    <>
       <h1>Codeitmall</h1>
-      <ul>
-        <li>
-          <Link href="/products/1">첫 번째 상품</Link>
-        </li>
-        <li>
-          <Link href="/products/2">두 번째 상품</Link>
-        </li>
-        <li>
-          <Link href="/products/3">세 번째 상품</Link>
-        </li>
-        <li>
-          <Link href="https://codeit.kr">코드잇</Link>
-        </li>
-      </ul>
-    </div>
+      <SearchForm />
+      <ProductList products={products} />
+    </>
   );
 }
