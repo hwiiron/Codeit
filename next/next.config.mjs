949729e0b460161db10ba8eb1f6ded205b8e2e27
Hwichul Shin <shinhwiiron@gmail.com>
2024-11-20 08:38:20 +0900

    Next 리다이렉트 적용

--- next/next.config.mjs
+++ next/next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/product/:id",
+        destination: "/items/:id",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
