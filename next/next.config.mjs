2f340fda9b4c263527dbafde90d0c150c92ad44c
Hwichul Shin <shinhwiiron@gmail.com>
2024-11-21 10:39:58 +0900

    Image 컴포넌트 실습 수업 코드 추가

--- next/next.config.mjs
+++ next/next.config.mjs
@@ -11,6 +11,17 @@ const nextConfig = {
       },
     ];
   },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "learn-codeit-kr-static.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+        pathname: "/codeitmall/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
