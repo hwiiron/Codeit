4179b37624e92e84fa080e8fc4a536793213adde
Hwichul Shin <shinhwiiron@gmail.com>
2024-11-21 16:18:29 +0900

    서버사이드렌더링 실습 코드 추가

--- onebite-next/section02/src/pages/index.tsx
+++ onebite-next/section02/src/pages/index.tsx
@@ -4,37 +4,39 @@ import { ReactNode } from "react";
 import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
 import { InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-Books";
 
-export const getServerSideProps = () => {
+export const getServerSideProps = async () => {
   // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
+  // const allBooks = await fetchBooks();
+  // const recoBooks = await fetchRandomBooks();
 
-  console.log("서버사이드 프롭스에요");
-
-  const data = "hello";
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
 
   return {
-    props: {
-      data,
-    },
+    props: { allBooks, recoBooks },
   };
 };
 
 export default function Home({
-  data,
+  allBooks,
+  recoBooks,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  console.log(data);
-
   return (
     <div className={styles.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {recoBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
