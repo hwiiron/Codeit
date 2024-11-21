b771e89719b640536eac77acfd9fe3143009c97f
Hwichul Shin <shinhwiiron@gmail.com>
2024-11-21 15:22:40 +0900

    사전렌더링과 데이터 패칭 수업 코드 추가

--- onebite-next/section02/src/pages/index.tsx
+++ onebite-next/section02/src/pages/index.tsx
@@ -1,13 +1,44 @@
 import SearchableLayout from "@/components/searchable-layout";
 import styles from "./index.module.css";
 import { ReactNode } from "react";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
+import { InferGetServerSidePropsType } from "next";
 
-export default function Home() {
+export const getServerSideProps = () => {
+  // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
+
+  console.log("서버사이드 프롭스에요");
+
+  const data = "hello";
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+export default function Home({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(data);
+
   return (
-    <>
-      <h1 className={styles.h1}>인덱스</h1>
-      <h2 className={styles.h2}>H2</h2>
-    </>
+    <div className={styles.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+    </div>
   );
 }
 
--- onebite-next/section02/src/pages/search/index.tsx
+++ onebite-next/section02/src/pages/search/index.tsx
@@ -1,12 +1,17 @@
+import BookItem from "@/components/book-item";
 import SearchableLayout from "@/components/searchable-layout";
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import books from "@/mock/books.json";
 
 export default function Page() {
-  const router = useRouter();
-  const { q } = router.query;
-
-  return <h1>Search {q}</h1>;
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
 
 Page.getLayout = (page: ReactNode) => {
