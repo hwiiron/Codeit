93e8485f31a2bf27c6487f958476624f30b23701
Hwichul Shin <shinhwiiron@gmail.com>
2024-11-21 17:24:01 +0900

    SSG 실습

--- onebite-next/section02/src/pages/index.tsx
+++ onebite-next/section02/src/pages/index.tsx
@@ -1,16 +1,18 @@
 import SearchableLayout from "@/components/searchable-layout";
 import styles from "./index.module.css";
 import { ReactNode } from "react";
-import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
-import { InferGetServerSidePropsType } from "next";
+import { InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-Books";
 
-export const getServerSideProps = async () => {
+// export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
   // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
   // const allBooks = await fetchBooks();
   // const recoBooks = await fetchRandomBooks();
+
+  console.log("인덱스 페이지");
 
   const [allBooks, recoBooks] = await Promise.all([
     fetchBooks(),
@@ -25,7 +27,8 @@ export const getServerSideProps = async () => {
 export default function Home({
   allBooks,
   recoBooks,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: // }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={styles.container}>
       <section>
--- onebite-next/section02/src/pages/search/index.tsx
+++ onebite-next/section02/src/pages/search/index.tsx
@@ -1,25 +1,41 @@
 import BookItem from "@/components/book-item";
 import SearchableLayout from "@/components/searchable-layout";
-import { ReactNode } from "react";
-import books from "@/mock/books.json";
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { ReactNode, useEffect, useState } from "react";
 import fetchBooks from "@/lib/fetch-books";
+import { useRouter } from "next/router";
+import { BookData } from "@/types";
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  const q = context.query.q;
+// export const getServerSideProps = async (
+// export const getStaticProps = async (context: GetStaticPropsContext) => {
+//   const q = context.query.q;
 
-  const books = await fetchBooks(q as string);
+//   const books = await fetchBooks(q as string);
 
-  return {
-    props: { books },
+//   return {
+//     props: { books },
+//   };
+// };
+
+// export default function Page({
+//   books,
+// }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Page() {
+  const [books, setBooks] = useState<BookData[]>([]);
+  const router = useRouter();
+  const q = router.query.q;
+
+  const fetchSearchResult = async () => {
+    const data = await fetchBooks(q as string);
+    setBooks(data);
   };
-};
 
-export default function Page({
-  books,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  useEffect(() => {
+    if (q) {
+      // 검색 결과를 불러오는 로직
+      fetchSearchResult();
+    }
+  }, [q]);
+
   return (
     <div>
       {books.map((book) => (
