f03fb75ff580de4b1a5bbe4bc8cfc2ae392b1d63
HwichulShin <shinhwiiron@gmail.com>
2025-01-06 14:44:28 +0900

    React-Query 수업 코드 추가

---
+++ react-query/src/components/PostList.js
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import { getPosts } from "../api";
+import Post from "./Post";
+import styles from "./PostList.module.css";
+
+function PostList() {
+  // 여기에 코드를 작성하세요
+  const { data } = useQuery({
+    queryKey: ["posts"],
+    queryFn: getPosts,
+  });
+
+  const posts = data?.results ?? [];
+
+  return (
+    <div className={styles.postList}>
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </div>
+  );
+}
+
+export default PostList;
